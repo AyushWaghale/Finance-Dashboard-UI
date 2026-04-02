0e23914594f959db77d1a9e87dd228201021dfc0
AYUSH WAGHALE <ayushwaghale777.com>
2026-04-02 21:42:08 +0530

    Add dashboard summary cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import './index.css'
 import Sidebar from "./components/layout/Sidebar";
 import Topbar from "./components/layout/Topbar";
+import SummaryCard from './components/cards/SummaryCard';
 
 function App() {
   return (
@@ -11,6 +12,12 @@ function App() {
         <Topbar />
         <div className="p-4">Dashboard Content</div>
       </div>
+
+      <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+  <SummaryCard title="Balance" amount="50000" />
+  <SummaryCard title="Income" amount="70000" />
+  <SummaryCard title="Expenses" amount="20000" />
+</div>
     </div>
   );
 }
